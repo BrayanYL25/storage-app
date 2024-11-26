c0401f6f0d28c3c94eca1997c85803102ccf8825
Brayan Yin Lin <brayanyl250206@gmail.com>
2024-11-26 00:02:43 -0500

    dev: ssl certificate for testing enviroment

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import * as fs from 'node:fs'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem')
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: [
