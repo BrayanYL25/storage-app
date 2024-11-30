43ea64491b583ba7a80d25a15c7e2271c11793c8
Brayan Yin Lin <brayanyl250206@gmail.com>
2024-11-29 21:11:12 -0500

    debug products endpoint

--- src/features/products/services/get_all_products.ts
+++ src/features/products/services/get_all_products.ts
@@ -13,7 +13,6 @@ export default async function getAllProducts() {
       throw new ErrorGettingProducts(`Hubo un error ${error.msg}`)
     }
     const products = await response.json()
-    console.log(products)
     const mappedProducts: Product[] = products.map(
       (product: any): Product => ({
         id: product.productId,
