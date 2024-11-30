33bfff7baba132b0a9926455789e306ea2a62c61
Brayan Yin Lin <brayanyl250206@gmail.com>
2024-11-29 21:33:00 -0500

    fix: base vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,6 @@ export default defineConfig({
       { find: '@', replacement: resolve(__dirname, 'src/shared') },
       { find: '#', replacement: resolve(__dirname, 'src/features') }
     ]
-  }
+  },
+  base: '/storage-app/'
 })
